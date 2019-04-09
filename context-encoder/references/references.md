801726a0acf599dda1fd61675d2dfe7985bcf264
Guillem96 <guillem.orellana@gmail.com>
2019-04-09 15:50:56 +0200

    Context Encoder: Initial commit

---
+++ context-encoder/references/references.md
@@ -0,0 +1,4 @@
+# References
+
+- [Knowledge base](https://arxiv.org/abs/1604.07379)
+- [Implementation](https://raw.githubusercontent.com/eriklindernoren/Keras-GAN/master/context_encoder/context_encoder.py)
---
+++ context-encoder/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An AI trained to generate the contents of an arbitrary image region conditioned on its surroundings. ',
+    author='Guillem96',
+    license='MIT',
+)
