ad73b7121a3fe06e58aad9b7fd99106943a57f16
Guillem96 <guillem.orellana@gmail.com>
2019-04-21 21:03:51 +0200

    Paint over an image

--- amethyst/lib/main.dart
+++ amethyst/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:amethyst/screens/home_screen/home_screen.dart';
 import 'package:amethyst/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(new MaterialApp(
-  home: new SplashScreen(),
+main() => runApp(MaterialApp(
+  home: SplashScreen(),
   routes: Router.routes,
 ));
--- amethyst/lib/screens/editor_screen/editor_screen.dart
+++ amethyst/lib/screens/editor_screen/editor_screen.dart
@@ -1,27 +1,179 @@
-
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:amethyst/screens/editor_screen/widgets/image_viewer.dart';
+import 'package:amethyst/screens/editor_screen/widgets/painter.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// class EditorScreen extends StatefulWidget {
+//   final File image;
+
+//   EditorScreen(this.image);
+
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _EditorScreenState();
+//   }
+// }
+
+// class _EditorScreenState extends State<EditorScreen> {
+
+//   _EditorScreenState();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: ImageViewer(widget.image),
+//     );
+//   }
+// }
 
 class EditorScreen extends StatefulWidget {
-  File image;
+  final File _image;
 
-  EditorScreen(this.image);
-
+  EditorScreen(this._image);
   @override
-  State<StatefulWidget> createState() {
-    return _EditorScreenState();
-  }
+  _EditorScreenState createState() => _EditorScreenState();
 }
 
 class _EditorScreenState extends State<EditorScreen> {
+  bool _finished;
+  PainterController _controller;
 
-  _EditorScreenState();
+  @override
+  void initState() {
+    super.initState();
+    _finished = false;
+    _controller = _Controller();
+  }
+
+  PainterController _Controller() {
+    PainterController controller = PainterController();
+    controller.thickness = 5.0;
+    controller.backgroundColor = Colors.transparent;
+    return controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ImageViewer(widget.image),);
+    List<Widget> actions;
+    if (_finished) {
+      actions = <Widget>[
+        IconButton(
+          icon: Icon(Icons.content_copy),
+          tooltip: ' Painting',
+          onPressed: () => setState(() {
+            _finished = false;
+            _controller = _Controller();
+          }),
+        ),
+      ];
+    } else {
+      actions = <Widget>[
+        IconButton(
+          icon: Icon(Icons.undo),
+          tooltip: 'Undo',
+          onPressed: _controller.undo
+        ),
+        IconButton(
+          icon: Icon(Icons.delete),
+          tooltip: 'Clear',
+          onPressed: _controller.clear
+        ),
+        IconButton(
+          icon: Icon(Icons.check),
+          onPressed: () async => _show(await _controller.finish(widget._image), context)
+        ),
+      ];
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Painter Example'),
+        actions: actions,
+        bottom: PreferredSize(
+          child: DrawBar(_controller),
+          preferredSize: Size(MediaQuery.of(context).size.width, 30.0),
+        )
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration( 
+            image: DecorationImage(
+              image: FileImage(widget._image),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Painter(_controller)
+        )
+      )
+    );
+  }
+
+  void _show(PictureDetails picture, BuildContext context) {
+    setState(() {
+      _finished = true;
+    });
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('View your image'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: FutureBuilder<Uint8List>(
+            future: picture.toPNG(),
+            builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Image.memory(snapshot.data);
+                  }
+                  break;
+                default:
+                  return Container(
+                      child: FractionallySizedBox(
+                    widthFactor: 0.1,
+                    child: AspectRatio(
+                        aspectRatio: 1.0, child: CircularProgressIndicator()),
+                    alignment: Alignment.center,
+                  ));
+              }
+            },
+          )
+        ),
+      );
+    }));
+  }
+}
+
+class DrawBar extends StatelessWidget {
+  final PainterController _controller;
+
+  DrawBar(this._controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Flexible(child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+          return Container(
+              child: Slider(
+            value: _controller.thickness,
+            onChanged: (double value) => setState(() {
+                  _controller.thickness = value;
+                }),
+            min: 1.0,
+            max: 20.0,
+            activeColor: Colors.white,
+          ));
+        })),
+      ],
+    );
   }
 }
